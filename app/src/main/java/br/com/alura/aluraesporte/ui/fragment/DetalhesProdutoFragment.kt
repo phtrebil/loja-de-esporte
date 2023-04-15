7f94715994fe6eae29b21288fbad520cc92e3298
phtrebil <phptrebil@gmail.com>
2023-04-15 17:53:35 -0300

    implementando navigation em todos os fragments

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
 import br.com.alura.aluraesporte.model.Produto
@@ -23,7 +24,8 @@ class DetalhesProdutoFragment : Fragment() {
             ?: throw IllegalArgumentException(ID_PRODUTO_INVALIDO)
     }
     private val viewModel: DetalhesProdutoViewModel by viewModel { parametersOf(produtoId) }
-    var quandoProdutoComprado: (produto: Produto) -> Unit = {}
+    private val controlador by lazy { findNavController() }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,8 +47,17 @@ class DetalhesProdutoFragment : Fragment() {
 
     private fun configuraBotaoComprar() {
         detalhes_produto_botao_comprar.setOnClickListener {
-            viewModel.produtoEncontrado.value?.let(quandoProdutoComprado)
+            viewModel.produtoEncontrado.value?.let {
+                vaiParaPagamento()
+            }
         }
+    }
+
+    private fun vaiParaPagamento() {
+        val controlador = findNavController()
+        val dados = Bundle()
+        dados.putLong(CHAVE_PRODUTO_ID, produtoId)
+        controlador.navigate(R.id.pagamento, dados)
     }
 
     private fun buscaProduto() {
--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaProdutosFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaProdutosFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import android.widget.LinearLayout.VERTICAL
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.model.Produto
+import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
 import br.com.alura.aluraesporte.ui.recyclerview.adapter.ProdutosAdapter
 import br.com.alura.aluraesporte.ui.viewmodel.ProdutosViewModel
 import kotlinx.android.synthetic.main.lista_produtos.*
@@ -20,8 +22,9 @@ class ListaProdutosFragment : Fragment() {
 
     private val viewModel: ProdutosViewModel by viewModel()
     private val adapter: ProdutosAdapter by inject()
-    var quandoProdutoSelecionado: (produto: Produto) -> Unit = {}
-
+    private val controlador by lazy {
+        findNavController()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         buscaProdutos()
@@ -54,8 +57,16 @@ class ListaProdutosFragment : Fragment() {
     private fun configuraRecyclerView() {
         val divisor = DividerItemDecoration(context, VERTICAL)
         lista_produtos_recyclerview.addItemDecoration(divisor)
-        adapter.onItemClickListener = quandoProdutoSelecionado
+        adapter.onItemClickListener = { produtoSelecionado ->
+            vaiParaDetalhes(produtoSelecionado)
+        }
         lista_produtos_recyclerview.adapter = adapter
+    }
+
+    private fun vaiParaDetalhes(produtoSelecionado: Produto) {
+        val dados = Bundle()
+        dados.putLong(CHAVE_PRODUTO_ID, produtoSelecionado.id)
+        controlador.navigate(R.id.detalhesProduto, dados)
     }
 
 }
