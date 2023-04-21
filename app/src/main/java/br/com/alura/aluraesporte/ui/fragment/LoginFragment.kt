8bf1aa82cb2949c7f45b9cd58a1d135d9f7349cc
phtrebil <phptrebil@gmail.com>
2023-04-21 07:53:10 -0300

    adicionando tela de login

---
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/LoginFragment.kt
@@ -0,0 +1,51 @@
+package br.com.alura.aluraesporte.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.login.*
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class LoginFragment : Fragment() {
+
+    private val viewModel: LoginViewModel by viewModel()
+
+    private val controlador by lazy {
+        findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.login,
+            container,
+            false
+        )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (viewModel.estaLogado()){
+            vaiParaListaDeProdutos()
+        }
+        login_botao_logar.setOnClickListener {
+            viewModel.loga()
+            vaiParaListaDeProdutos()
+        }
+    }
+
+    private fun vaiParaListaDeProdutos() {
+        val direcao = LoginFragmentDirections.actionLoginToListaProdutos()
+        controlador.navigate(direcao)
+    }
+
+}
